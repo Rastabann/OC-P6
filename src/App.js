46b8ad1ff8cb7e46ae763f7f1797f4d21d6c62bf
Rastabann <alexis.godin1207@gmail.com>
2023-07-17 15:58:33 +0200

    Projet init et routing

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./css/app.css";
+
+import Routing from "./components/Routing";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routing />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
